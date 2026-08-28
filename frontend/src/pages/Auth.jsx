2e15d03e671b0911eae313aeaec7b3cd1f7531b7
lissa-986 <lissaa0021gmail.com>
2026-08-28 15:55:12 +0500

    update serveo live url

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -20,7 +20,7 @@ const Auth = () => {
   const [error, setError] = useState('');
 
   // Backend Live URL (Serveo Tunnel URL)
-  const API_BASE = "https://a0c7ab1e8bd767d5-154-192-215-40.serveousercontent.com";
+  const API_BASE = "https://aeccb58157de51ce-154-192-215-40.serveousercontent.com";
 
   const handleSendOtp = async (e) => {
     e.preventDefault();
